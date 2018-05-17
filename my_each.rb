ef5a84c5bf12ff5c723967d3d411eafdac160492
Robert Tabares <robtabares92@gmail.com>
2018-05-17 20:48:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  n=0;
+  while n<arr.length do
+    yield(arr[n])
+    n+=1
+  end
+  return arr
+end
+
+my_each(collection) do |i|
+  puts i
 end
